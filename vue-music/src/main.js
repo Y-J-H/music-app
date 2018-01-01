5faa8e20d6ecf3bb0ca115acd5cc67aa1b65e195
YJH <m17854239497@outlook.com>
2018-01-01 13:45:47 +0800

    添加babel-polyfill, fastclick插件

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -1,10 +1,14 @@
+import 'babel-polyfill'     // 解决不支持es6语法的问题
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import fastclick from 'fastclick'      // 解决移动端点击事件存在300毫秒延迟的问题
+
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
