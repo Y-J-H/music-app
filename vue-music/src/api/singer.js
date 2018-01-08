b6b37fef15f757975e6422b1a9a93fd2486ad390
YJH <m17854239497@outlook.com>
2018-01-08 16:10:20 +0800

    获取歌手数据,并格式化

---
+++ vue-music/src/api/singer.js
@@ -0,0 +1,19 @@
+import jsonp from 'common/js/jsonp'
+import { commonParams, options } from 'api/config'
+
+export function getSinger () {
+  let url = 'https://szc.y.qq.com/v8/fcg-bin/v8.fcg'
+  let data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    loginUin: 0,
+    hostUin: 0,
+    notic: 0,
+    platform: 'yqq',
+    needNewCode: 0
+  })
+  return jsonp(url, data, options)
+}
--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -5,8 +5,81 @@
 </template>
 
 <script>
-export default {
+import { getSinger } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
 
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
+export default {
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSinger()
+  },
+  methods: {
+    _getSinger () {
+      return getSinger().then((res) => {
+        console.log(res)
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+          console.log(this.singers)
+        }
+      })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        // 分类热门数据
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_id,
+            mid: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        // 以A,B,C...来分类数据
+        if (!map[item['Findex']]) {
+          map[item.Findex] = {
+            title: item.Findex,
+            list: []
+          }
+        }
+
+        map[item.Findex].list.push(new Singer({
+          id: item.Fsinger_id,
+          mid: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+
+        // 将map中的数据序列化
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        console.log(arguments)
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  }
 }
 </script>
 
