47a75b78d83495292101d4b5b67cdc609b43de40
YJH <m17854239497@outlook.com>
2018-01-13 15:21:34 +0800

    添加歌手详情页面的歌手名称，歌手图片

--- vue-music/src/components/singer-detail/singer-detail.vue
+++ vue-music/src/components/singer-detail/singer-detail.vue
@@ -1,8 +1,6 @@
 <template>
 <transition name="slide">
-  <div class="singer-detail">
-    歌手详情页
-  </div>
+  <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
 </transition>
 </template>
 
@@ -11,9 +9,21 @@ import { mapGetters } from 'vuex'
 import { getSingerDetail } from 'api/singer'
 import { ERR_OK } from 'api/config'
 import { createSong } from 'common/js/song'
+import musicList from 'components/music-list/music-list'
 
 export default {
+  data () {
+    return {
+      songs: []
+    }
+  },
   computed: {
+    title () {
+      return this.singer.name
+    },
+    bgImage () {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
@@ -41,21 +51,15 @@ export default {
       })
       return ret
     }
+  },
+  components: {
+    musicList
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
