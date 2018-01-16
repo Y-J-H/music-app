6cfe577510b216f267d1cf7097dff00987322ebe
YJH <m17854239497@outlook.com>
2018-01-16 14:50:33 +0800

    给player组件填充数据

--- vue-music/src/store/action.js
+++ vue-music/src/store/action.js
@@ -1,7 +1,6 @@
 import * as types from './mutation-types'
 // 当在一个动作中需要改变多个mutations的时候我们就需要封装一个action
 export const selectPlay = function ({commit, state}, {list, index}) {
-  console.log(arguments)
   commit(types.SET_SEQUENCE_LIST, list)
   commit(types.SET_PLAYLIST, list)
   commit(types.SET_CURRENT_INDEX, index)
